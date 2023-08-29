f947f05e6a87707c960ea56645ec730283c25f92
Maksym Cherniavskiy <morfey897@gmail.com>
2023-08-29 17:19:25 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,10 @@ import "server-only";
 import Image from 'next/image'
 import { currentLocale } from 'next-i18n-router';
 
-import { headers } from 'next/headers';
-
 export default function Home(props: any) {
   const locale = currentLocale();
-  const propsJS = JSON.stringify(props);
-  const headersJS = JSON.stringify(headers().entries())
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>propsJS: {propsJS}</div>
-      <div>headersJS: {headersJS}</div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
