0f9a5a720b3057cda4049846abafe18b757c5e1c
Maksym Cherniavskyi <morfey897@gmail.com>
2023-08-29 12:36:25 +0300

    Created error component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,44 +1,32 @@
-// app/not-found.tsx
-import Link from 'next/link';
+'use client' // Error components must be Client Components
+ 
+import { useEffect } from 'react'
 
 export const runtime = 'edge';
-
-export default function Error() {
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
   return (
-    <div className='w-4/5 mx-auto mt-20 flex flex-col justify-center items-center space-y-4'>
-      <h1 className='text-4xl font-semibold'>Something went wrong</h1>
-      <h4 className='py-10 text-lg text-red-500 font-semibold capitalize'>
-        The Nothingness and Emptiness is here...
-      </h4>
-      <p>
-        Use the search box or the links below to explore our amazing
-        application
-      </p>
-      <input
-        className='w-4/5 px-6 py-3 border border-gray-400 rounded-full'
-        type='search'
-        placeholder='Just a dummy search box...'
-      />
-      <div className='space-x-4'>
-        <Link
-          className='underline text-blue-600 hover:text-red-500 duration-300'
-          href='/'
-        >
-          Homepage
-        </Link>
-        <Link
-          className='underline text-blue-600 hover:text-red-500 duration-300'
-          href='/latest'
-        >
-          Latest Products
-        </Link>
-        <Link
-          className='underline text-blue-600 hover:text-red-500 duration-300'
-          href='/contact'
-        >
-          Contact Us
-        </Link>
-      </div>
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
     </div>
-  );
+  )
 }
