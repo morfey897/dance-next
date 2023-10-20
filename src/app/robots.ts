96ab27b644bfcb5877e5d266dd03c770f182fb17
Maksym Cherniavskyi <morfey897@gmail.com>
2023-10-20 13:01:31 +0300

    Fixed sitemap.xml & robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_DOMAIN}/sitemap.xml`,
   }
 }
