55f6725053a49b727e004fb30c3ea2f9ffa6a447
Maksym Cherniavskyi <morfey897@gmail.com>
2023-10-24 10:58:41 +0300

    Image optimization

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -7,6 +7,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import dictionary from '@/i18n/error-copy.json';
 import Heading from '@/components/elements/Headline/Heading';
+import { ChevronDown } from '@/components/elements/Icons';
 
 export default function Error({
   error,
@@ -35,21 +36,7 @@ export default function Error({
             () => reset()
           } className="group space-x-2 block w-fit m-auto mt-7 text-sm md:text-lg text-center border-2 border-pnk-200 rounded-3xl py-2 px-4 hover:bg-pnk-200 hover:border-pnk-200 hover:shadow hover:shadow-pnk-200 active:border-pnk-100 active:shadow active:shadow-pnk-100">
             <span className="inline-block">{t('subheadline')}</span>
-            <svg className="animate-bounce inline-block fill-pnk-100 group-hover:animate-none" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 512 512" xmlSpace="preserve">
-              <g>
-                <g>
-                  <g>
-                    <path d="M256,0C114.618,0,0,114.618,0,256s114.618,256,256,256s256-114.618,256-256S397.382,0,256,0z M256,469.333
-				c-117.818,0-213.333-95.515-213.333-213.333S138.182,42.667,256,42.667S469.333,138.182,469.333,256S373.818,469.333,256,469.333
-				z"/>
-                    <path d="M347.582,198.248L256,289.83l-91.582-91.582c-8.331-8.331-21.839-8.331-30.17,0c-8.331,8.331-8.331,21.839,0,30.17
-				l106.667,106.667c8.331,8.331,21.839,8.331,30.17,0l106.667-106.667c8.331-8.331,8.331-21.839,0-30.17
-				C369.42,189.917,355.913,189.917,347.582,198.248z"/>
-                  </g>
-                </g>
-              </g>
-            </svg>
+            <ChevronDown />
           </button>
         </Article>
       </main>
--- src/components/blocks/About/index.tsx
+++ src/components/blocks/About/index.tsx
@@ -7,6 +7,7 @@ import { SectionType } from "@/models/page";
 import Section from "@/components/elements/Section";
 import { findImage } from "@/utils/filter";
 import Headline from "@/components/elements/Headline";
+import { ChevronDown } from "@/components/elements/Icons";
 
 function About({ headline, body, anchor, callToAction, images }: SectionType) {
 
@@ -14,10 +15,10 @@ function About({ headline, body, anchor, callToAction, images }: SectionType) {
   const image = findImage(images, '!bg');
 
   return <>
-    <div className="absolute inset-0 z-0">
+    <div className="absolute inset-0 z-0 hidden lg:block">
       <Asset asset={bgImage} alt={''} className="[&>img]:h-full [&>img]:w-full" />
     </div>
-    <Article className="text-center pt-[74px]" anchor={anchor?.tag}>
+    <Article className="text-center pt-[74px]" anchor={anchor?.tag} effect={{ x: 'center', y: 'center' }}>
       <div className="relative z-10">
         <Asset asset={image} quality={100} alt={headline} className="[&>img]:m-auto md:[&>img]:p-0 [&>img]:px-10 brightness-75" />
         <Section animate="b-t">
@@ -26,21 +27,7 @@ function About({ headline, body, anchor, callToAction, images }: SectionType) {
           </Headline>
           <CTA cta={callToAction} className="group space-x-2 block w-fit m-auto mt-7 text-sm md:text-lg text-center border-2 border-pnk-200 rounded-3xl py-2 px-4 hover:bg-pnk-200 hover:border-pnk-200 hover:shadow hover:shadow-pnk-200 active:border-pnk-100 active:shadow active:shadow-pnk-100">
             <span className="inline-block">{callToAction?.title}</span>
-            <svg className="animate-bounce inline-block fill-pnk-100 group-hover:animate-none" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 512 512" xmlSpace="preserve">
-              <g>
-                <g>
-                  <g>
-                    <path d="M256,0C114.618,0,0,114.618,0,256s114.618,256,256,256s256-114.618,256-256S397.382,0,256,0z M256,469.333
-				c-117.818,0-213.333-95.515-213.333-213.333S138.182,42.667,256,42.667S469.333,138.182,469.333,256S373.818,469.333,256,469.333
-				z"/>
-                    <path d="M347.582,198.248L256,289.83l-91.582-91.582c-8.331-8.331-21.839-8.331-30.17,0c-8.331,8.331-8.331,21.839,0,30.17
-				l106.667,106.667c8.331,8.331,21.839,8.331,30.17,0l106.667-106.667c8.331-8.331,8.331-21.839,0-30.17
-				C369.42,189.917,355.913,189.917,347.582,198.248z"/>
-                  </g>
-                </g>
-              </g>
-            </svg>
+            <ChevronDown />
           </CTA>
         </Section>
       </div>
