38cfd166203f9ac90b0b144648dc4d11c46f22c4
Maksym Cherniavskyi <morfey897@gmail.com>
2023-11-09 15:48:35 +0200

    Created 404 & error pages

--- apps/crm/src/app/not-found.tsx
+++ apps/crm/src/app/not-found.tsx
@@ -1,7 +1,12 @@
+import NotFound from "@/views/NotFound";
+
 export default async function Page() {
 
-  return (<main>
-    <p className='text-4xl'>404</p>
-  </main>
-  )
+  return <main>
+    <section className="bg-white dark:bg-gray-900 ">
+      <div className="container flex items-center justify-center min-h-screen px-6 py-12 mx-auto">
+        <NotFound />
+      </div>
+    </section>
+  </main>;
 }
--- apps/crm/src/views/Aside/index.tsx
+++ apps/crm/src/views/Aside/index.tsx
@@ -19,7 +19,7 @@ function Aside() {
   const activeClassName = "!bg-gray-200 dark:!bg-gray-700";
 
   return <div className='flex-shrink-0 h-screen w-14 md:w-32 lg:w-64'>
-    <aside className="top-0 bottom-0 z-10 left-0 fixed flex flex-col w-14 md:w-32 lg:w-64 lg:px-4 py-4 lg:py-8  bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
+    <aside className="top-0 bottom-0 z-10 left-0 fixed flex flex-col w-14 md:w-32 lg:w-64 lg:px-4 py-4 lg:py-8  bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-800 dark:border-gray-700">
       <nav className="flex flex-col flex-1 space-y-2 md:space-y-6 pt-[80px]">
 
         <NavItem href={routes.ROOT} label={t("root")} Icon={HiHome} className={clsx(isActive(pathName, routes.ROOT) && activeClassName)} />
