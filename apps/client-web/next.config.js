dcf0ce9ca098730235377f2b4a83c3f592bee1bf
Maksym Cherniavskyi <morfey897@gmail.com>
2023-11-11 16:03:02 +0200

    Get rid of crm

--- apps/client-web/next.config.js
+++ apps/client-web/next.config.js
@@ -7,6 +7,7 @@ const withPWAInit = require("@ducanh2912/next-pwa");
 /** @type {import('next-pwa').PWAConfig} */
 const withPWA = withPWAInit.default({
   dest: 'public',
+  disable: process.env.NODE_ENV !== "production",
   fallbacks: {
     document: "/~offline",
   }
